89a0a33c7eca9eb89b0d83fbfef460793ea94da8
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-07-09 15:01:58 +0300

    add solution to the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const priceCar = 40;
+  const fullPrice = priceCar * days;
+
+  if (days < 3) {
+    return fullPrice;
+  }
+
+  if (days >= 3 && days < 7) {
+    return fullPrice - 20;
+  }
+
+  if (days >= 7) {
+    return fullPrice - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
